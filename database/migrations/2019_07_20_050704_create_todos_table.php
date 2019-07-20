352ca520b0aa798dec660cc7e11163b16a2314c2
lindelea <kgo@lindelin.org>
2019-07-20 14:55:05 +0900

    Database init

--- database/migrations/2019_07_20_050704_create_todos_table.php
+++ database/migrations/2019_07_20_050704_create_todos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTodosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->uuid('id');
-            $table->string('name');
-            $table->string('icon_path');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->uuid('user_id');
+            $table->string('title');
+            $table->string('icon_name');
+            $table->date('started_at');
+            $table->date('ended_at');
+            $table->integer('priority');
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('todos');
     }
 }
